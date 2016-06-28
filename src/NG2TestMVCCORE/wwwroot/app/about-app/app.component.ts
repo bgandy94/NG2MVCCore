c380185b7b44ae142b02d02c379937964a66f7a1
Brandon Gandy <brandongundy@gmail.com>
2016-06-28 13:34:32 -0500

    Checkin for scoping issue on if statement in about app.

--- src/NG2TestMVCCORE/wwwroot/app/about-app/app.component.ts
+++ src/NG2TestMVCCORE/wwwroot/app/about-app/app.component.ts
@@ -3,10 +3,15 @@
     selector: 'about-app',
     template: `<h2>Hello, {{title}}</h2>
                <input [(ngModel)]="title" placeholder="name" />
+                <h2 *ngIf="showH2">*NGIF IS WORKING!</h2>
 `
 })
 export class AppComponent {
-
     title: string;
+    showH2: boolean = false;
 
+    if(this.title.length > 0){
+
+    
+}
 }

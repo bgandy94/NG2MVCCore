8e78c6620191a003a72d77205f3ddf05e18750f3
Brandon Gandy <brandongundy@gmail.com>
2016-06-29 13:18:13 -0500

    Cleaned libs to make app smaller for pull requests and added more code to the about-app.

--- src/NG2TestMVCCORE/wwwroot/app/about-app/app.component.ts
+++ src/NG2TestMVCCORE/wwwroot/app/about-app/app.component.ts
@@ -1,17 +1,15 @@
 ﻿import {Component} from '@angular/core';
 @Component({
     selector: 'about-app',
-    template: `<h2>Hello, {{title}}</h2>
-               <input (keyup)="checkTitle()" [(ngModel)]="title" placeholder="name" />
-                <h2 *ngIf="showH2">*NGIF IS WORKING!</h2>
-`
+    templateUrl: './app/about-app/app.component.html'
 })
 export class AppComponent {
     title: string = "";
+    title2: string = "";
     showH2: boolean = false;
 
-    checkTitle = function() {
-        if (this.title.length > 0) {
+    checkTitles = function() {
+        if (this.title.length > 0 && this.title2.length > 0) {
             this.showH2 = true;
         } else {
             this.showH2 = false;
